5707522bca4c552411f13c9629756729a3041cf5
Víctor de Lara Martínez <vdelara.afd@gmail.com>
2023-08-14 16:45:20 +0200

    Add model data

---
+++ src/model/country_type.ts
@@ -0,0 +1,13 @@
+export type CountryType = {
+  name: string;
+  continent: string;
+  flag: string;
+  coatOfArmy: string;
+  capital: string;
+  language: string;
+  demonym: string;
+  currencies: string;
+  population: number;
+  timezone: string;
+  maps: string;
+};
