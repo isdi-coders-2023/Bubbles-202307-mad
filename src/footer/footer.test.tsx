6d2453dfa0961283ad30fd46a96d71ef67118605
yolanda fdez <iolandafdez@gmail.com>
2023-08-14 21:29:09 +0200

    Add footer2

--- src/footer/footer.test.tsx
+++ src/footer/footer.test.tsx
@@ -7,7 +7,7 @@ describe('Given the component Footer', () => {
   describe('When we render it', () => {
     render(<Footer></Footer>);
     test('the component should be in the document footer', () => {
-      const element = screen.getByText('Around the world');
+      const element = screen.getByText('Around the World');
       expect(element).toBeInTheDocument();
     });
   });
