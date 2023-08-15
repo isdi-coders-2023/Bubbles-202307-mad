c4e38c3b4778a45e2b7ab69e354bc6db326b689b
Miguel Angel Izquierdo <izquierdomiguela@gmail.com>
2023-08-15 12:09:39 +0200

    adapt mock in test Card

--- src/component/card/card.test.tsx
+++ src/component/card/card.test.tsx
@@ -3,12 +3,20 @@ import { render, screen } from '@testing-library/react';
 import { Card } from './card';
 describe('Given the component card', () => {
   describe('When we render it', () => {
-    const mockProps = {
-      name: '',
-      flag: '',
-      continent: '',
+    const mockCountry = {
+      name: 'Saudi Arabia',
+      continent: 'Asia',
+      flag: 'https://flagcdn.com/w320/sa.png',
+      coatOfArmy: 'https://mainfacts.com/media/images/coats_of_arms/sa.png',
+      capital: 'Riyadh',
+      language: 'Arabic',
+      demonym: 'Saudi Arabian',
+      currencies: 'Central African CFA franc',
+      population: 26545864,
+      timezone: 'UTC+03:00',
+      maps: 'https://goo.gl/maps/5PSjvdJ1AyaLFRrG9',
     };
-    render(<Card {...mockProps}></Card>);
+    render(<Card key={0} country={mockCountry}></Card>);
     test('Then the component should be in the document', () => {});
     const element = screen.getByRole('listitem');
     expect(element).toBeInTheDocument();
