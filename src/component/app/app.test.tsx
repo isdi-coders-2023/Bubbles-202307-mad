424fba622f6261e9d638c632306122ae07a32a46
yolanda fdez <iolandafdez@gmail.com>
2023-08-15 19:55:16 +0200

    Add router to test

--- src/component/app/app.test.tsx
+++ src/component/app/app.test.tsx
@@ -1,10 +1,15 @@
 import '@testing-library/jest-dom';
 import { render, screen } from '@testing-library/react';
+import { BrowserRouter as Router } from 'react-router-dom';
 import App from './app';
 
 describe('Given the component App', () => {
   describe('When we render it', () => {
-    render(<App />);
+    render(
+      <Router>
+        <App />
+      </Router>
+    );
     test('Then component should be in the document', () => {
       const element = screen.getByRole('heading');
       const text = screen.getByText('countries of the world');
--- src/component/menu/menu.test.tsx
+++ src/component/menu/menu.test.tsx
@@ -1,10 +1,14 @@
 import '@testing-library/jest-dom';
 import { render, screen } from '@testing-library/react';
+import { BrowserRouter as Router } from 'react-router-dom';
 import { Menu } from './menu';
-
 describe('Given the componente Menu', () => {
   describe('When we render it', () => {
-    render(<Menu></Menu>);
+    render(
+      <Router>
+        <Menu></Menu>
+      </Router>
+    );
 
     test('the component should be in the document', () => {
       const element = screen.getByText('Your countries');
