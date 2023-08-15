5a42527e0dcb67d95e5a387de9b1f33121f1560b
yolanda fdez <iolandafdez@gmail.com>
2023-08-15 12:20:16 +0200

    Add style page_controler

--- src/component/page_controler/page_controler.test.tsx
+++ src/component/page_controler/page_controler.test.tsx
@@ -1,1 +1,13 @@
-//ahora lo hago
+import '@testing-library/jest-dom';
+import { render, screen } from '@testing-library/react';
+import { PageControler } from './page_controler';
+
+describe('Given the component page_controler', () => {
+  describe('When we render it', () => {
+    render(<PageControler></PageControler>);
+    test('the component should be in the document', () => {
+      const element = screen.getByRole('page_controler');
+      expect(element).toBeInTheDocument();
+    });
+  });
+});
--- src/component/page_controler/page_controler.tsx
+++ src/component/page_controler/page_controler.tsx
@@ -1,9 +1,10 @@
+import styles from './page_controler.module.scss';
 export function PageControler() {
   return (
-    <div>
-      <button>PREVIOUS</button>
-      <span>20/50</span>
-      <button>NEXT</button>
+    <div className={styles.buttonBox}>
+      <button className={styles.previousButton}>PREVIOUS</button>
+      <span className={styles.span}>20/50</span>
+      <button className={styles.nextButton}>NEXT</button>
     </div>
   );
 }
