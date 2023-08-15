ee305afe4428e5533b71b8a157cddd1ffae0dc2c
Miguel Angel Izquierdo <izquierdomiguela@gmail.com>
2023-08-15 12:04:50 +0200

    Add countries component in app

--- src/component/countries/countries.tsx
+++ src/component/countries/countries.tsx
@@ -1,8 +1,14 @@
-import style from './countries.module.scss';
+import { arrCountries } from '../../service/countries_data';
+import { Card } from '../card/card';
+import styles from './countries.module.scss';
 export function Countries() {
   return (
-    <section className={style.section}>
-      <ol></ol>
+    <section className={styles.section}>
+      <ol className={styles.ol}>
+        {arrCountries.map((item, index) => (
+          <Card key={index} country={item}></Card>
+        ))}
+      </ol>
     </section>
   );
 }
