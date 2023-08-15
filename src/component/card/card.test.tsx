30ceee01aa68564bcf7718bc9bd270f32651e766
Víctor de Lara Martínez <vdelara.afd@gmail.com>
2023-08-15 12:20:51 +0200

    Edit card.test

--- src/component/card/card.test.tsx
+++ src/component/card/card.test.tsx
@@ -11,6 +11,9 @@ describe('Given the component card', () => {
     render(<Card {...mockProps}></Card>);
     test('Then the component should be in the document', () => {});
     const element = screen.getByRole('listitem');
+    // const name = screen.getByText('');
+    // const flag = screen.getByText('');
+    // const continent = screen.getByText('');
     expect(element).toBeInTheDocument();
   });
 });
