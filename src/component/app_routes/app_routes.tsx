dc80811b9817d2e92464e67c98e1ed8f7cbd3c73
Miguel Angel Izquierdo <izquierdomiguela@gmail.com>
2023-08-15 20:32:54 +0200

    Implement route dinamic from link

--- src/component/app_routes/app_routes.tsx
+++ src/component/app_routes/app_routes.tsx
@@ -1,6 +1,8 @@
 import { Suspense, lazy } from 'react';
 import { Route, Routes } from 'react-router-dom';
 import { MenuOption } from '../../model/menu_options';
+import { arrCountries } from '../../service/countries_data';
+import { Info } from '../info/info';
 
 const Home = lazy(() => import('../../pages/home/home'));
 // const Favorites = lazy(() => import(''));
@@ -16,6 +18,10 @@ export const AppRoutes = ({ options }: Props) => {
       <Routes>
         <Route path="/" element={<Home></Home>}></Route>
         <Route path="*" element={<Home></Home>}></Route>
+        <Route
+          path="/home/:product"
+          element={<Info country={arrCountries[0]}></Info>}
+        />
         <Route path={paths[0]} element={<Home></Home>}></Route>
         <Route path={paths[1]} element={<></>}></Route>
         <Route path={paths[2]} element={<></>}></Route>
--- src/component/card/card.tsx
+++ src/component/card/card.tsx
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom';
 import { CountryType } from '../../model/country_type';
 import styles from './card.module.scss';
 type Props = {
@@ -6,9 +7,11 @@ type Props = {
 export const Card = (country: Props) => {
   return (
     <li className={styles.card}>
-      <span className={styles.name}>{country.country.name}</span>
-      <img className={styles.flag} src={country.country.flag} />
-      <span className={styles.continent}>{country.country.continent}</span>
+      <Link className={styles.link} to={`/home/:${country.country.name}`}>
+        <span className={styles.name}>{country.country.name}</span>
+        <img className={styles.flag} src={country.country.flag} />
+        <span className={styles.continent}>{country.country.continent}</span>
+      </Link>
     </li>
   );
 };
