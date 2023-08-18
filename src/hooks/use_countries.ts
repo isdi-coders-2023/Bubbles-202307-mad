de73e59f9c170abf00aab718ef360ec9f5e4d741
Miguel Angel Izquierdo <izquierdomiguela@gmail.com>
2023-08-18 16:22:48 +0200

    Implement function assignPaginated in useCountries

--- src/hooks/use_countries.ts
+++ src/hooks/use_countries.ts
@@ -10,6 +10,7 @@ import { ApiRepository } from '../service/repository/api_repository';
 
 const urlBase = 'https://restcountries.com/v3.1/all';
 let allCountries: CountryType[] = [];
+let currentPage = 1;
 export function useCountries() {
   const repo = useMemo(() => new ApiRepository(urlBase), []);
   const navigate = useNavigate();
@@ -33,7 +34,9 @@ export function useCountries() {
     dispatch2(ac.loadCardInfoActionCreater(country));
   };
   const filterByContinent = (continent: string) => {
+    assignPaginated();
     if (continent === 'All') {
+      assignPaginated();
       return dispatch(ac.filterByContinentCreater(allCountries));
     }
     const filteredCountries = allCountries.filter(
@@ -41,6 +44,23 @@ export function useCountries() {
     );
     dispatch(ac.filterByContinentCreater(filteredCountries));
   };
+
+  const assignPaginated = () => {
+    const countriesPerPage = 10;
+    let totalCountries = 0;
+    let currentPage = 1;
+    allCountries = allCountries.map((country) => {
+      if (totalCountries > 9) {
+        totalCountries = 0;
+        currentPage++;
+      } else {
+        totalCountries++;
+      }
+      country.page = currentPage;
+      return country;
+    });
+  };
+
   return {
     loadAllCountries,
     loadCountryInfo,
