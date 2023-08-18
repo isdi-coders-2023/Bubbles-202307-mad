56cb4814e650a93cf4c19394e06d8c8c79c8d356
yolanda fdez <iolandafdez@gmail.com>
2023-08-18 11:56:45 +0200

    Add form to add country

--- src/component/form_add_countries/form_add_countries.tsx
+++ src/component/form_add_countries/form_add_countries.tsx
@@ -1,8 +1,9 @@
 import { useState } from 'react';
 import { Countries } from '../countries/countries';
+import { Form } from '../form_/form';
 
 export type NewCountry = {
-  country: string;
+  Imaginarycountry: string;
   flagImage: string;
   language: string;
   currencies: string;
@@ -35,44 +36,46 @@ function handleChange(): void {
 
 const newCountries = {
   ...Countries,
-  [add_country]: Countries,
+  [Form]: Countries,
 }
 
 return (
- <form action="/my-handling-form-page" method="post">
-
-  <ul>
+   <div>
+    <h1>Invented new country</h1>
+   <form action="/my-invented-form-page" 
+   method="post">
+<ul>
     <li>
       <label for="country">Country:</label>
-      <input type="text" id="country" name="user_country" />
+      <input type="text" id="country" name="user_country"  onChange={(event) => this.useState({name: event.target.value})} />
     </li>
     <li>
       <label for="flag-image">Flag Image:</label>
-      <input type="img.png" id="flag" name="user_flag-image" />
+      <input type="img.png" id="flag" name="user_flag-image" handleSubmit=""/>
     </li>
     <li>
-      <label for="language">Language:</label>
-      <input id="text" country="user_language"></input>
+      <label for="language">Language or signal language:</label>
+      <input id="text" country="user_language" handleSubmit= ""></input>
     </li>
       <li>
-      <label for="currencies">Currencies:</label>
-      <input id="text" country="user_currencies"></input>
+      <label for="currencies">Currencies or barter:</label>
+      <input id="text" country="user_currencies" handleSubmit=""></input>
     </li>
       <li>
       <label for="timeZone">Time Zone:</label>
-      <input id="number" country="user_timeZone"></input>
+      <input id="number" country="user_timeZone" handleSubmit=""></input>
     </li>
       <li>
       <label for="continent">Continent:</label>
-      <input id="text" country="user_continent"></input>
+      <input id="text" country="user_continent" handleSubmit=""></input>
     </li>
       <li>
       <label for="capital">Capital:</label>
-      <input id="text" country="user_capital"></input>
+      <input id="text" country="user_capital" handleSubmit=""></input>
     </li>
        <li>
       <label for="demonym">Demonym:</label>
-      <input id="demonym" country="user_demonym"></input>
+      <input id="demonym" country="user_demonym" handleSubmit=""></input>
     </li>
       <li>
       <label for="population"> Population:</label>
@@ -83,9 +86,11 @@ return (
       <input id="url" country="user_mapUrl"></input>
     </li>
   </ul>
+  
   <p><button type="submit">Add Country</button></p>
 </form>
-
-
+</div> 
 )
 }
+
+
