804dc034ad15177bf21088add92afc7f5fedacbe
Víctor de Lara Martínez <vdelara.afd@gmail.com>
2023-08-20 15:24:26 +0200

    Add a mocked array for ApiRepository test

--- src/service/repository/api_repository.test.ts
+++ src/service/repository/api_repository.test.ts
@@ -1,3 +1,4 @@
+import { mockArrCountries } from '../countries_data';
 import { ApiRepository } from './api_repository';
 
 describe('Given the class ApiRepository', () => {
@@ -7,7 +8,7 @@ describe('Given the class ApiRepository', () => {
     test('Then we expected getAll method to be called', () => {
       global.fetch = jest.fn().mockResolvedValueOnce({
         ok: true,
-        json: jest.fn().mockResolvedValue('test'),
+        json: jest.fn().mockResolvedValue(mockArrCountries),
       });
       mockRepo.getAll();
       expect(global.fetch).toHaveBeenCalled();
