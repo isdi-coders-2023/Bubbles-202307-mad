176157ac9d7efd0d3bd13a36ba29af3f286aa83f
Miguel Angel Izquierdo <izquierdomiguela@gmail.com>
2023-08-20 18:52:23 +0200

    Modify test info component

--- src/component/info/info.test.tsx
+++ src/component/info/info.test.tsx
@@ -2,21 +2,24 @@ import '@testing-library/jest-dom';
 import { render, screen } from '@testing-library/react';
 import { MemoryRouter as Router } from 'react-router-dom';
 import { AppContext, AppContextStructure } from '../../context/app_context';
+import { CountryType } from '../../model/country_type';
 import { Info } from './info';
 describe('Given the component Info', () => {
-  const mockCountry = {
-    name: 'Saudi Arabia',
-    continents: ['Asia'],
-    flag: 'https://flagcdn.com/w320/sa.png',
-    coatOfArm: 'https://mainfacts.com/media/images/coats_of_arms/sa.png',
-    capital: { eng: { eng: 'English' } },
-    languages: 'Arabic',
-    demonym: 'Saudi Arabian',
-    currencies: null,
-    population: 26545864,
-    timezone: 'UTC+03:00',
-    maps: 'https://goo.gl/maps/5PSjvdJ1AyaLFRrG9',
-  };
+  const mockCountry = [
+    {
+      name: 'Saudi Arabia',
+      continents: ['Asia'],
+      flag: 'https://flagcdn.com/w320/sa.png',
+      coatOfArm: 'pepe',
+      capital: { eng: { eng: 'English' } },
+      languages: 'Arabic',
+      demonym: 'Saudi Arabian',
+      currencies: null,
+      population: 26545864,
+      timezone: 'UTC+03:00',
+      maps: 'https://goo.gl/maps/5PSjvdJ1AyaLFRrG9',
+    },
+  ] as unknown as CountryType;
   describe('When we render it', () => {
     const mockContextValue: AppContextStructure = {
       countriesContext: {
@@ -25,6 +28,8 @@ describe('Given the component Info', () => {
         },
       },
     } as unknown as AppContextStructure;
+    mockContextValue.countriesContext.countryInfo.coatOfArm = 'pepe';
+    mockContextValue.countriesContext.countryInfo.name = 'pepe';
     render(
       <AppContext.Provider value={mockContextValue}>
         <Router>
@@ -34,7 +39,9 @@ describe('Given the component Info', () => {
     );
     test('Then component should be in the document', () => {
       const element = screen.getByRole('button');
+      const text = screen.getByText('pepe');
       expect(element).toBeInTheDocument();
+      expect(text).toBeInTheDocument();
     });
   });
 });
