c70dd3c4cbed829c13cb4f7bace72d64ddea2a76
Miguel Angel Izquierdo <izquierdomiguela@gmail.com>
2023-08-20 17:56:23 +0200

    Modify test info component

--- src/component/info/info.test.tsx
+++ src/component/info/info.test.tsx
@@ -1,16 +1,36 @@
 import '@testing-library/jest-dom';
 import { render, screen } from '@testing-library/react';
 import { MemoryRouter as Router } from 'react-router-dom';
-import { AppContextProvider } from '../../context/app_context_provider';
+import { AppContext, AppContextStructure } from '../../context/app_context';
 import { Info } from './info';
-describe('Given the component Header', () => {
+describe('Given the component Info', () => {
+  const mockCountry = {
+    name: 'Saudi Arabia',
+    continents: ['Asia'],
+    flag: 'https://flagcdn.com/w320/sa.png',
+    coatOfArm: 'https://mainfacts.com/media/images/coats_of_arms/sa.png',
+    capital: { eng: { eng: 'English' } },
+    languages: 'Arabic',
+    demonym: 'Saudi Arabian',
+    currencies: null,
+    population: 26545864,
+    timezone: 'UTC+03:00',
+    maps: 'https://goo.gl/maps/5PSjvdJ1AyaLFRrG9',
+  };
   describe('When we render it', () => {
+    const mockContextValue: AppContextStructure = {
+      countriesContext: {
+        countryInfo: {
+          mockCountry,
+        },
+      },
+    } as unknown as AppContextStructure;
     render(
-      <Router>
-        <AppContextProvider>
+      <AppContext.Provider value={mockContextValue}>
+        <Router>
           <Info></Info>
-        </AppContextProvider>
-      </Router>
+        </Router>
+      </AppContext.Provider>
     );
     test('Then component should be in the document', () => {
       const element = screen.getByRole('button');
